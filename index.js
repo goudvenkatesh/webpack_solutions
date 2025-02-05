d5cc9656fe20c7c21b006ea761f23d60283b8568
venkatesh <venkatesh@example.com>
2025-02-05 22:35:35 +0530

    adding javascript functionality

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let textEl=document.getElementById('text');
 let readMoreBtnEl=document.getElementById('readButton');
+let previousBtnEl=document.getElementById('previousBtn');
+let nextBtnEl=document.getElementById('nextBtn');
+let sliderTextEl=document.getElementById('sliderText');
 
 
 let fullText="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting in-dustry"
@@ -17,3 +20,20 @@ function onToggleText(){
     }
     isCollapse=!isCollapse;
 }
+
+let slidersText=["Dr.Reddy's","HETERO","BHARAT","BIOCON"]
+let currentIndex=0;
+
+function updateSlider(){
+    sliderTextEl.textContent=slidersText[currentIndex]
+}
+
+previousBtnEl.addEventListener("click",()=>{
+    currentIndex=(currentIndex-1+slidersText.length) % slidersText.length;
+    updateSlider()
+})
+
+nextBtnEl.addEventListener("click",()=>{
+    currentIndex=(currentIndex+1) % slidersText.length;
+    updateSlider()
+})
